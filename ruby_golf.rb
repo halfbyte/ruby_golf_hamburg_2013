f3a516a7f2a35dc8cdca23cd5c6b785b593f385e
Jan Krutisch <jan@krutisch.de>
2013-11-13 20:58:19 +0100

    one less

--- ruby_golf.rb
+++ ruby_golf.rb
@@ -8,6 +8,8 @@ module RubyGolf
   #         v - number of visible trailing numbers
   # output: x'ed out string
   def self.x_out_numbers(n, v)
+    p = (n.length - v)
+    p>0 ? n.gsub(/\d{#{p}}/, 'x' * p) : n
   end
 
 
@@ -18,6 +20,7 @@ module RubyGolf
   # ouput:  a 'ruby style' version of the identifier: all lowercase, former case
   #         changes to upper case get a prepended underscore
   def self.underscore(s)
+    s.gsub(/([a-z])-*([A-Z])/, '\1_\2').downcase
   end
 
 
@@ -30,6 +33,10 @@ module RubyGolf
   #         Values that are hashes contain only smybols as keys too, this
   #         condition is maintained recursivley
   def self.symbolize_keys(h)
+    return h if !h.is_a?(Hash)
+    o={}
+    h.each {|k,v| o[k.to_sym] = self.symbolize_keys(v)}
+    o
   end
 
 
@@ -40,7 +47,13 @@ module RubyGolf
   #         ending in a \n
   # output: the maximum value found by calculating the sums of all rows and
   #         columns
+  
   def self.grid_computing(g)
+    a = g.split("\n").map{|s| s.split(" ")}
+    (a + a.transpose).inject(0) {|y,l| 
+      s=l.inject(0){|m,n| m+n.to_i}
+      s>y ? s : y
+    }
   end
 
 
@@ -52,6 +65,7 @@ module RubyGolf
   # output: a string describing the same hash but without hash rockets, but
   #         otherwise with the same formatting
   def self.reformat_hash(s)
+    s.gsub(/:{1}([a-z]+):{0} \=\>/, '\1:')
   end
 
 
@@ -67,7 +81,8 @@ module RubyGolf
   #         * all keys or values that are contained in a hash that is not top
   #           level are prepended by two additional spaces per level away from
   #           the top level
-  def self.pretty_hash(h)
+  def self.pretty_hash(h, d=0)
+    a = h.map{|k,v| (" " * d) + k.to_s + ":\n" + (v.is_a?(Hash) ? pretty_hash(v,d+2) : [v].flatten.map{|a| (" " * d) + "- " + "#{a}" + "\n"}.join())}.join()
   end
 
 
@@ -85,6 +100,11 @@ module RubyGolf
   #           word 1* ...)
   #         * sum all products
   def self.word_letter_sum(s)
+    x = 0
+    s.upcase.split().map{|w|
+      w.each_char.inject(0) {|m,c| m + (c.ord - 64) }
+    }.sort.each_with_index{|w,i| x += (i + 1) * w }
+    x
   end
 
 
@@ -98,6 +118,15 @@ module RubyGolf
   # output: an ascii art string ready for output where there aren't any trailing
   #         spaces after the last character in each line
   def self.bob_ross(s)
+    b=[]
+    a=s.split(/\n/).map{|l| l.split(" ")}
+    a.map{|c| 
+      x,y,z = c
+      b[y.to_i] ||= []
+      b[y.to_i][x.to_i] = z.to_i
+    }
+    b.map{|l| l.map{ |c| c ? c.chr : ' '}.join.rstrip + "\n"}.join
+    
   end
 
 end
